3693407b76f0874dfef51571aa368526b222cccf
Om More <51229945+thisisommore@users.noreply.github.com>
2021-10-28 19:35:14 +0530

    Introduce API for deleting User (#8)

---
+++ app/tests/deleteuser_test.go
@@ -0,0 +1,52 @@
+package tests
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/thisisommore/go-user-app-backend/app"
+	"github.com/thisisommore/go-user-app-backend/db"
+	"github.com/thisisommore/go-user-app-backend/user"
+	"github.com/thisisommore/go-user-app-backend/util"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func TestDeleteUser(t *testing.T) {
+	usersCollection := db.Initialize().Collection("Users")
+	userId := primitive.NewObjectID()
+	newUser := user.User{
+		ID:          userId,
+		Name:        "Doremon",
+		Dob:         "10 july",
+		Address:     "Mangaon",
+		Description: "Great user",
+		CreatedAt:   time.Now().String(),
+	}
+	_, err := usersCollection.InsertOne(context.TODO(), newUser)
+	util.HandleTestError(err, t)
+
+	url := "/user/" + userId.Hex()
+	router := app.CreateRouter()
+	rr := httptest.NewRecorder()
+	request, err := http.NewRequest("DELETE", url, nil)
+	util.HandleTestError(err, t)
+	router.ServeHTTP(rr, request)
+	if statusCode := rr.Result().StatusCode; statusCode != http.StatusOK {
+		t.Fatal(statusCode)
+	}
+
+	findResErr := usersCollection.FindOne(context.TODO(), bson.M{"_id": userId}).Err()
+	if findResErr == nil {
+		t.Fatal("User still exist")
+	}
+	if findResErr != mongo.ErrNoDocuments {
+		t.Fatal(findResErr)
+	}
+	usersCollection.DeleteOne(context.TODO(), bson.M{"_id": userId})
+
+}
